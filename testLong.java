644fe6acaa9582ae68065a38c0218efc7f4759d0
King Ballo <stouvoli@gmail.com>
2026-03-17 11:48:44 +0000

    Create testLong.java

---
+++ testLong.java
@@ -0,0 +1,6 @@
+@org.junit.jupiter.api.Test
+public void testLong() throws InterruptedException {
+    System.out.println("Début du sommeil...");
+    Thread.sleep(10000); // 10 secondes
+    System.out.println("Fin du sommeil.");
+}
