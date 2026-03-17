abe9e3aedc2f0f809eaf442d97c74775f27fd4ca
King Ballo <stouvoli@gmail.com>
2026-03-17 13:55:30 +0000

    Create TimeoutTest.java

---
+++ TimeoutTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class TimeoutTest {
+    @Test
+    public void testInfini() {
+        System.out.println("Début du test infini...");
+        while(true) {
+            // L'étudiant a écrit une boucle infinie ici
+        }
+    }
+}
